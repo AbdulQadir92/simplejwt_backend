b170d70e8ca8c642662b2b2e95bcaea79b43cb99
AbdulQadir92 <a_qadir_jamali@hotmail.com>
2022-11-01 13:10:29 -0700

    Added login functionality

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
 from . import views
+from .views import MyTokenObtainPairView
+
+from rest_framework_simplejwt.views import (
+    TokenRefreshView
+)
 
 urlpatterns = [
-    path('register/', views.register_user)
+    path('register/', views.register_user),
+    path('api/token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
